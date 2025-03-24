31c68590ff69d3ceacaa929936eca341dafc998e
David Godswill <davidilerioluwa1998@gmail.com>
2025-03-24 23:13:49 +0100

    j

--- src/App.jsx
+++ src/App.jsx
@@ -10,19 +10,19 @@ export default function App() {
 const [scale,setScale]=React.useState(1)
 const [print,setPrint]=React.useState(false)
   return (
-    <>
-     <div className={`flex flex-col ${print?"":"gap-10"} p-4`} ref={componentRef} style={{transform:`scale(${scale})`}}>
-   <div className="print-container">
+    <div className="bg-stone-900">
+     <div className={`flex flex-col ${print?"":"gap-4"} p-4`} ref={componentRef} style={{transform:`scale(${scale})`}}>
+   <div className="print-container drop-shadow-lg">
      <img src="T1.png"/>
    </div>
    <div className="page-break"></div>
 
-   <div className="print-container">
+   <div className="print-container drop-shadow-lg">
      <img src="T2.png"/>
    </div>
    <div className="page-break"></div>
 
-   <div className="print-container">
+   <div className="print-container drop-shadow-lg">
      <img src="T3.png"/>
  </div>
  </div>
@@ -41,9 +41,9 @@ const [print,setPrint]=React.useState(false)
       handlePrint()
     }, 100);
    }} className="text-sm h-fit cursor-pointer">Download</span> */}
-   <a href="/example.pdf" className="text-sm h-fit cursor-pointer pt-2" download={"TOSCANA.pdf"}>Download</a>
+   <a href="/" className="text-sm h-fit cursor-pointer pt-2" download={"TOSCANA.pdf"}>Download</a>
   </div>
 </div>
-    </>
+    </div>
   );
 }
