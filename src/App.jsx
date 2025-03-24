ed499ccf5aa6f0e5950df67f00f6570aab257443
David Godswill <davidilerioluwa1998@gmail.com>
2025-03-24 22:47:03 +0100

    initial

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import * as React from "react";
+import { useReactToPrint } from "react-to-print";
+
+export default function App() {
+  const componentRef = React.useRef(null);
+  const handlePrint =useReactToPrint({
+    contentRef: componentRef,
+    documentTitle: "TOSCANA MENU",
+  });
+const [scale,setScale]=React.useState(1)
+const [print,setPrint]=React.useState(false)
+  return (
+    <>
+     <div className={`flex flex-col ${print?"":"gap-10"} p-4`} ref={componentRef} style={{transform:`scale(${scale})`}}>
+   <div className="print-container">
+     <img src="T1.png"/>
+   </div>
+   <div className="page-break"></div>
+
+   <div className="print-container">
+     <img src="T2.png"/>
+   </div>
+   <div className="page-break"></div>
+
+   <div className="print-container">
+     <img src="T3.png"/>
+ </div>
+ </div>
+<div className="fixed bottom-20 left-0 flex justify-center items-center w-screen">
+<div className=" bg-white drop-shadow-md py-3 px-3 rounded-lg flex gap-8 text-amber-950 font-bold text-3xl items-center"> 
+   <span className="cursor-pointer" onClick={()=>{
+    setScale(scale+0.2)
+   }}>+</span>
+   <span className="cursor-pointer" onClick={()=>{
+    setScale(scale-0.2)
+   }}>-</span>
+   {/* <span onClick={()=>{
+    setScale(1)
+    setPrint(true)
+    setTimeout(() => {
+      handlePrint()
+    }, 100);
+   }} className="text-sm h-fit cursor-pointer">Download</span> */}
+   <a href="/example.pdf" className="text-sm h-fit cursor-pointer pt-2" download={"TOSCANA.pdf"}>Download</a>
+  </div>
+</div>
+    </>
+  );
+}
